500ffa7efae3513e918defa8ed3fc0aead0cea53
Maks <maks35@gmail.com>
2025-04-13 12:03:41 +0300

    №1 and №2

---
+++ Serialization/Serialization/ArrayOfFractions.cs
@@ -0,0 +1,77 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Text.Json;
+
+//namespace FractionSerializationApp
+//{
+   
+//    [Serializable]
+//    public class Fraction
+//    {
+//        public int Numerator { get; set; }
+//        public int Denominator { get; set; }
+
+//        public override string ToString()
+//        {
+//            return $"{Numerator}/{Denominator}";
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // 1 Введення масиву  з клавіатури
+//            List<Fraction> fractions = new List<Fraction>();
+
+//            Console.Write("Скільки дробів ви хочете ввести? ");
+//            int count = int.Parse(Console.ReadLine());
+
+//            for (int i = 0; i < count; i++)
+//            {
+//                Console.WriteLine($"\nДріб {i + 1}:");
+
+//                Console.Write("  Введіть чисельник: ");
+//                int numerator = int.Parse(Console.ReadLine());
+
+//                Console.Write("  Введіть знаменник: ");
+//                int denominator = int.Parse(Console.ReadLine());
+
+//                fractions.Add(new Fraction
+//                {
+//                    Numerator = numerator,
+//                    Denominator = denominator
+//                });
+//            }
+
+//            // 2 Серіалізація масиву дробів у формат JSON
+//            string json = JsonSerializer.Serialize(fractions, new JsonSerializerOptions { WriteIndented = true });
+
+//            // 3 Збереження серіалізованого масиву у файл
+//            string fileName = "fractions.json";
+//            File.WriteAllText(fileName, json);
+//            Console.WriteLine($"\nМасив дробів збережено у файл: {fileName}");
+
+//            // 4 Завантаження та десеріалізація з файлу
+//            if (File.Exists(fileName))
+//            {
+//                string loadedJson = File.ReadAllText(fileName);
+//                List<Fraction> loadedFractions = JsonSerializer.Deserialize<List<Fraction>>(loadedJson);
+
+//                Console.WriteLine("\nЗавантажені дроби з файлу:");
+//                foreach (var frac in loadedFractions)
+//                {
+//                    Console.WriteLine("  " + frac);
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Файл не знайдено!");
+//            }
+
+//            Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ Serialization/Serialization/magazine.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.IO;
+//using System.Text.Json;
+
+//namespace JournalTask2
+//{
+//    public class Journal
+//    {
+//        public string Title { get; set; }
+//        public string Publisher { get; set; }
+//        public DateTime ReleaseDate { get; set; }
+//        public int PageCount { get; set; }
+
+//        public override string ToString()
+//        {
+//            return $"Назва: {Title}\nВидавництво: {Publisher}\nДата: {ReleaseDate.ToShortDateString()}\nСторінок: {PageCount}";
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Journal journal = new Journal();
+
+//            Console.Write("Назва журналу: ");
+//            journal.Title = Console.ReadLine();
+
+//            Console.Write("Назва видавництва: ");
+//            journal.Publisher = Console.ReadLine();
+
+//            Console.Write("Дата випуску (рррр-мм-дд): ");
+//            journal.ReleaseDate = DateTime.Parse(Console.ReadLine());
+
+//            Console.Write("Кількість сторінок: ");
+//            journal.PageCount = int.Parse(Console.ReadLine());
+
+//            // Серіалізація
+//            string json = JsonSerializer.Serialize(journal, new JsonSerializerOptions { WriteIndented = true });
+//            File.WriteAllText("journal.json", json);
+//            Console.WriteLine("\nЖурнал збережено у файл journal.json");
+
+//            // Десеріалізація
+//            string loadedJson = File.ReadAllText("journal.json");
+//            Journal loadedJournal = JsonSerializer.Deserialize<Journal>(loadedJson);
+
+//            Console.WriteLine("\nЗавантажений журнал:");
+//            Console.WriteLine(loadedJournal);
+
+//            Console.WriteLine("\nНатисніть будь-яку клавішу...");
+//            Console.ReadKey();
+//        }
+//    }
+//}
